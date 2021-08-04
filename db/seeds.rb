35765f0dfdf4ddb0f87573d77e85015f9801b7d4
Romain Losey <romain.losey@gmail.com>
2021-08-04 15:24:49 +0200

    Finished exercise

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,20 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+require 'faker'
+
+puts 'Creating 10 fake flats...'
+
+10.times do
+  flat = Flat.create!(
+  name: "#{Faker::Name.name}'s flat",
+  address: Faker::Address.full_address,
+  description: Faker::Lorem.words,
+  price_per_night: (10..100).to_a.sample,
+  number_of_guests: (1..10).to_a.sample
+)
+  flat.save
+end
+puts 'Finished!'
